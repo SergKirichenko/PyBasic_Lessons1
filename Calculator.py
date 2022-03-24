b4e138d20c7ebb0975dce26fd719d3c4f8cfba12
Serg Kirichenko <ss86kirichenko@gmail.com>
2022-03-24 18:58:56 +0200

    First task - HomeWork_Calculator v1.2.1 First task - Calculator v2.0.1

--- Calculator.py
+++ Calculator.py
@@ -9,12 +9,12 @@ half_res = None
 first_val = input(" Input First number: ")
 second_val = input(" Input Second number: ")
 operator = input("Сhoose the operation:\n 1: +\n 2: -\n 3: *\n 4: /\n 5: ^\n  Operation: ")
-operator_f = {"1": "+", "2": "-", "3": "*", "4": "/", "5": "^"}
+operator_f = {"1": "+", "2": "-", "3": "*", "4": "/", "5": "^"}                         # библиотека
 ##############################################################################################
 try:
     first_in = float(first_val)
     second_in = float(second_val)
-    half_res = f"{first_in}{operator_f[operator]}{second_in} = "
+    half_res = f"{first_in} {operator_f[operator]} {second_in} = "                      # для красоты
 except (NameError, KeyError, ValueError, TypeError):
     print("!!!Error!!! - Input Correct Value /  Не правилный ввод данных")
 try:
@@ -23,9 +23,9 @@ try:
     result_3 = first_in * second_in
     result_4 = first_in / second_in
     result_5 = first_in ** second_in
-    result = (result_1, result_2, result_3, result_4, result_5)
+    result = (result_1, result_2, result_3, result_4, result_5)                          # список
     operator_r = int(operator)
-    print("Result: ", half_res, result[operator_r - 1])
+    print("\n  Result: ", half_res, result[operator_r - 1])                              # срез списка
 except (NameError, KeyError, ValueError, TypeError, ZeroDivisionError, IndexError):
     print("!!!Error!!! - Input Correct Value /  Не правилный ввод / Деление на 0")
 ################################################################################################

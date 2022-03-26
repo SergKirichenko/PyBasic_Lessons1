c1dbff1ebc6bf69fd68cd8d2a3f0d4d56040a26a
Serg Kirichenko <ss86kirichenko@gmail.com>
2022-03-26 17:54:21 +0200

    Tasks for Lessons 4 Task 6 for lesson 4 Calculator v2.1 HomeWork Calculator v1.3

--- HomeWork_Calc.py
+++ HomeWork_Calc.py
@@ -1,42 +1,52 @@
 print(" Hello!\nЗадание калкулятор")
 #############################################################################################
 half_res = None
+loop_op = True
 ##############################################################################################
-first_num = input(" Input First number: ")
-operator = input("Сhoose the operation:\n 1: +\n 2: -\n 3: *\n 4: /\n 5: ^\n  Operation: ")
-second_num = input(" Input Second number: ")
-operator_f = {"1": "+", "2": "-", "3": "*", "4": "/", "5": "^"}       # вне классная работа для красоты
+while loop_op:
+
+    first_num = input(" Input First number: ")
+    operator = input("Сhoose the operation:\n 1: +\n 2: -\n 3: *\n 4: /\n 5: ^\n  Operation: ")
+    second_num = input(" Input Second number: ")
+    operator_f = {"1": "+", "2": "-", "3": "*", "4": "/", "5": "^"}       # вне классная работа для красоты
 ###############################################################################################
-try:
-    first_in = float(first_num)
-    second_in = float(second_num)
-    operator_a = int(operator)
-except(TypeError, KeyError, NameError, ValueError):
-    print("!!!Error!!! - Choose Correct Operator")
-try:
-    half_res = f"{first_in} {operator_f[operator]} {second_in} = "  #
-except (TypeError, KeyError, NameError, ValueError):
-    print("!!!Error!!! - Choose Correct Operator /  Не правилный ввод ")
-##############################################################################################
-try:
-    if operator == "1":                               # операция слложения
-        result = first_in + second_in
-        print(f"Result :\n  {half_res} {result}")
-    elif operator == "2":                              # операция вычетания
-        result = first_in - second_in
-        print(f"Result :\n  {half_res} {result}")
-    elif operator == "3":                              # операция умножения
-        result = first_in * second_in
-        print(f"Result :\n  {half_res} {result}")
-    elif operator == "4":                              # операция деления
-        result = first_in / second_in
-        print(f"Result :\n  {half_res} {result}")
-    elif operator == "5":                              # операция возведения в степень
-        result = first_in ** second_in
-        print(f"Result :\n  {half_res} {result}")
-    else:
+    try:
+        operator_a = int(operator)
+    except(TypeError, KeyError, NameError, ValueError):
         print("!!!Error!!! - Choose Correct Operator")
-except (NameError, ZeroDivisionError, TypeError):
-    print("!!!Error!!! - раскладка клавиатуры | Не правельный ввод данный | Деление на 0")
+
+    try:
+        first_in = float(first_num)
+        second_in = float(second_num)
+
+    except(TypeError, KeyError, NameError, ValueError):
+        print("!!!Error!!! - Input Correct Number")
+    try:
+        half_res = f"{first_in} {operator_f[operator]} {second_in} = "  #
+    except (TypeError, KeyError, NameError, ValueError):
+        print("!!!Error!!! -   Не правилный ввод ")
+##############################################################################################
+    try:
+        if operator == "1":                               # операция слложения
+            result = first_in + second_in
+            print(f"Result :\n  {half_res} {result}")
+        elif operator == "2":                              # операция вычетания
+            result = first_in - second_in
+            print(f"Result :\n  {half_res} {result}")
+        elif operator == "3":                              # операция умножения
+            result = first_in * second_in
+            print(f"Result :\n  {half_res} {result}")
+        elif operator == "4":                              # операция деления
+            result = first_in / second_in
+            print(f"Result :\n  {half_res} {result}")
+        elif operator == "5":                              # операция возведения в степень
+            result = first_in ** second_in
+            print(f"Result :\n  {half_res} {result}")
+        else:
+            print("!!!Error!!! - Choose 1-5 Operator")
+    except (NameError, ZeroDivisionError, TypeError, OverflowError):
+        print("!!!Error!!! - раскладка клавиатуры | Не правельный ввод данный | Деление на 0 | Очень болшое число")
 ###################################################################################
+    loop = input("  Press Any Key to try again. Press 'n' to quit: ")
+    loop_op = True if loop != "n" else False
 print(" The End ")

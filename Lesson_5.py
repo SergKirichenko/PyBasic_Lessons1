a531f0dc1a911794a5450c1c5f37b789fae5cfcc
Serg Kirichenko <ss86kirichenko@gmail.com>
2022-03-30 10:53:59 +0300

    Tasks for Lessons 4 Task 6 for lesson 4 Calculator v2.21 HomeWork Calculator v1.3 Lesson_5

--- Lesson_5.py
+++ Lesson_5.py
@@ -3,10 +3,8 @@ new_str_1 = '0123456789'
 for stm_1 in new_str_1:
     for stm_2 in new_str_1:
         result = stm_1 + stm_2
-        if result.isalnum():
-            result = int(result)
-            print(result)
+        result = int(result)
+        print(result)
 
-            # new_result.append(result)    # можно собрать данные в список. но этого в задание не было.
-
+        # new_result.append(result)    # можно собрать данные в список. но этого в задание не было.
 # print(new_result)                        #

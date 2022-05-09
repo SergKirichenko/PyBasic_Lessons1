76c3f9ac06f5bbd94a6b73a1659b03f2b4aa9ab2
Serg Kirichenko <ss86kirichenko@gmail.com>
2022-05-09 19:35:22 +0300

    Tasks for Lessons 4 Task 6 for lesson 4 Calculator v2.21 HomeWork Calculator v1.3 Lesson_5 Lesson_6 Lesson_7 [13q) Lesson_8 Lesson_9 lesson_10 + 3 Files + 2nd var Lesson_11 + Dir/files Project_trader

--- trader/trader.py
+++ trader/trader.py
@@ -10,24 +10,24 @@ args = ArgumentParser()
 args.add_argument("command", type=str)
 args.add_argument("second", nargs="?", default="")
 args = vars(args.parse_args())
-key = args['command'].upper()
+key_ = args['command'].upper()
 sub_key = args['second']
 score = 0
 if sub_key.isalpha():
-    keys = f"{key} {sub_key.upper()}"
+    keys = f"{key_} {sub_key.upper()}"
 elif sub_key.isdigit():
-    keys = key
+    keys = key_
     try:
         score = float(sub_key)
     except TypeError:
         print("Enter correct number!")
 else:
-    keys = key
+    keys = key_
 
 
 #####################################################################################################################
 # Ф-ции чтения и записи файл-статуса
-def read_json_file(filename_) -> Union[Dict]:
+def read_json_file(filename_: str) -> Union[Dict]:
     with open(filename_, 'r') as file:
         data = json.load(file)
     return data
@@ -39,10 +39,7 @@ def write_json_file(data: Union[List, Dict], filename_: str = "config_logs.json"
 
 
 ####################################################################################################################
-# Ф-ции записи логов
-def stat_logos(rate_: float = 0, delta_: float = 0, money_usd_: float = 0, money_uah_: float = 0, clear=False):
-    path_file = os.path.join("logs.csv")
-    date_time = datetime.datetime.now().strftime("%d-%m-%y %X")
+def logs_read(path_file) -> Union[list, int]:
     data = []
     with open(path_file, 'r', newline='') as file:
         reader = csv.reader(file, delimiter='|')
@@ -50,11 +47,28 @@ def stat_logos(rate_: float = 0, delta_: float = 0, money_usd_: float = 0, money
         for row in reader:
             data.append(row)
             nom += 1
-    header = data[0]
-    data_info = [nom, rate_, delta_, money_usd_, money_uah_, date_time]
+    return [data, nom]
+
+
+def logs_write(path_file, data_):
     with open(path_file, 'a', newline='') as file:
         writer = csv.writer(file, delimiter='|')
-        writer.writerow(data_info)
+        writer.writerow(data_)
+
+
+# Ф-ции записи логов
+def stat_logos(rate_: float = 0, delta_: float = 0, money_usd_: float = 0, money_uah_: float = 0):
+    path_file = os.path.join("logs.csv")
+    date_time = datetime.datetime.now().strftime("%d-%m-%y %X")
+    data_list, nom = logs_read(path_file)
+    data_info = [nom, rate_, delta_, money_usd_, money_uah_, date_time]
+    logs_write(path_file=path_file, data_=data_info)
+
+
+def clear_logs(clear=False):
+    path_file = os.path.join("logs.csv")
+    data_list, nom = logs_read(path_file)
+    header = data_list[0]
     if clear:
         os.remove(path_file)
         with open(path_file, 'a', newline='') as file:
@@ -63,26 +77,34 @@ def stat_logos(rate_: float = 0, delta_: float = 0, money_usd_: float = 0, money
 
 
 #######################################################################################################################
-
-# ф-ция покупки n-го кол-ва валюты
-def buy_usd(input_buy: float, filename_: str = "config_logs.json"):
-    data = read_json_file(filename_)
+def data_read(data: dict):
     rate_ = data["rate"]
     money_usd_ = data["amount on account USD"]
     money_uah_ = data["amount on account UAH"]
+    delta_ = data["delta"]
+    return [rate_, money_usd_, money_uah_, delta_]
+
+
+def data_write(data_: dict, rate_: float, money_usd_: float, money_uah_: float, delta: float) -> dict:
+    data_["rate"] = rate_
+    data_["amount on account USD"] = round(money_usd_, 2)
+    data_["amount on account UAH"] = round(money_uah_, 2)
+    data_["delta"] = delta
+    return data_
+
+
+######################################################################################################################
+# ф-ция покупки n-го кол-ва валюты
+def buy_usd(input_buy: float, filename_: str = "config_logs.json"):
+    data = read_json_file(filename_)
+    rate_, money_usd_, money_uah_, delta_ = data_read(data)
     deal = input_buy * rate_
-    assert deal < money_uah_
     if (money_uah_ - deal) > 0:
         money_uah_ -= round(deal, 2)
         money_usd_ += input_buy
-        data["amount on account USD"] = round(money_usd_, 2)
-        data["amount on account UAH"] = round(money_uah_, 2)
-        print(f"Deal Successful! You Buy: {input_buy},USD; Balance money: {round(money_uah_, 2)},"
-              f" UAH and {round(money_usd_, 2)}, USD ")
+        data = data_write(data_=data, rate_=rate_, money_usd_=money_usd_, money_uah_=money_uah_, delta=delta_)
     else:
-        deal = round((money_uah_ / rate_), 2)
-        print(f"Not enough money in the account. You can buy only: {deal}, USD.")
-
+        print(f"UNAVAILABLE, REQUIRED BALANCE UAH: {round(deal, 2)}, AVAILABLE {round(money_uah_, 2)}")
     write_json_file(data)
     stat_logos(rate_=rate_, money_usd_=money_usd_, money_uah_=money_uah_)
 
@@ -90,16 +112,11 @@ def buy_usd(input_buy: float, filename_: str = "config_logs.json"):
 # Покупка ВСЁ
 def buy_all(filename_: str = "config_logs.json"):
     data = read_json_file(filename_)
-    rate_ = data["rate"]
-    money_usd_ = data["amount on account USD"]
-    money_uah_ = data["amount on account UAH"]
+    rate_, money_usd_, money_uah_, delta_ = data_read(data)
     deal = money_uah_ / rate_
     money_usd_ += round(deal, 2)
     money_uah_ -= round((deal * rate_), 2)
-    print(f"Deal Successful! You Buy: {round(deal, 2)},USD; Balance money: {round(money_uah_, 2)},"
-          f" UAH and {round(money_usd_, 2)}, USD ")
-    data["amount on account USD"] = money_usd_
-    data["amount on account UAH"] = money_uah_
+    data = data_write(data_=data, rate_=rate_, money_usd_=money_usd_, money_uah_=money_uah_, delta=delta_)
     write_json_file(data)
     stat_logos(rate_=rate_, money_usd_=money_usd_, money_uah_=money_uah_)
 
@@ -107,20 +124,14 @@ def buy_all(filename_: str = "config_logs.json"):
 # Ф-ция продажи кол-ва USD
 def sell_usd(input_sell: float, filename_: str = "config_logs.json"):
     data = read_json_file(filename_)
-    rate_ = data["rate"]
-    money_usd_ = data["amount on account USD"]
-    money_uah_ = data["amount on account UAH"]
-    assert input_sell <= money_usd_
+    rate_, money_usd_, money_uah_, delta_ = data_read(data)
     if input_sell <= money_usd_:
         deal = input_sell * rate_
         money_uah_ += round(deal, 2)
         money_usd_ -= round(input_sell, 2)
-        print(f"Deal Successful! You Sell: {input_sell},USD; Balance money: {round(money_uah_, 2)}, "
-              f"UAH and {round(money_usd_, 2)}, USD ")
-        data["amount on account USD"] = money_usd_
-        data["amount on account UAH"] = money_uah_
+        data = data_write(data_=data, rate_=rate_, money_usd_=money_usd_, money_uah_=money_uah_, delta=delta_)
     else:
-        print("Input Correct Number!!!")
+        print(f"UNAVAILABLE, REQUIRED BALANCE USD: {round(input_sell, 2)}, AVAILABLE {round(money_usd_, 2)}")
     write_json_file(data)
     stat_logos(rate_=rate_, money_usd_=money_usd_, money_uah_=money_uah_)
 
@@ -128,17 +139,11 @@ def sell_usd(input_sell: float, filename_: str = "config_logs.json"):
 #  Ф-ция Продажа На ВСЕ
 def sell_all(filename_: str = "config_logs.json"):
     data = read_json_file(filename_)
-    rate_ = data["rate"]
-    money_usd_ = data["amount on account USD"]
-    money_uah_ = data["amount on account UAH"]
-    first = data["amount on account USD"]
+    rate_, money_usd_, money_uah_, delta_ = data_read(data)
     deal = money_usd_ * rate_
     money_uah_ += round(deal, 2)
     money_usd_ -= round((deal / rate_), 2)
-    print(f"Deal Successful! You Sell: {first},USD; Balance money: {round(money_uah_, 2)},"
-          f" UAH and {round(money_usd_, 2)}, USD ")
-    data["amount on account USD"] = money_usd_
-    data["amount on account UAH"] = money_uah_
+    data = data_write(data_=data, rate_=rate_, money_usd_=money_usd_, money_uah_=money_uah_, delta=delta_)
     write_json_file(data)
     stat_logos(rate_=rate_, money_usd_=money_usd_, money_uah_=money_uah_)
 
@@ -149,37 +154,34 @@ def available_info(filename_: str = "config.json") -> None:
     dict_status = read_json_file(filename_)
     money_usd_ = dict_status["amount on account USD"]
     money_uah_ = dict_status["amount on account UAH"]
-    print(f"Your Balance: on UAH account: {round(money_uah_, 2)}, UAH\n"
-          f" {' ' * 13} on USD account: {round(money_usd_, 2)}, USD")
+    print(f"USD {round(money_uah_, 2)} UAH {round(money_usd_, 2)}")
 
 
 # Ф-ция Инфо Курса валюты
 def rate_val(filename_: str = "config.json") -> None:
     dict_status = read_json_file(filename_)
     rate_ = dict_status["rate"]
-    print(f"Now rate is: {rate_}")
+    print(f"{rate_}")
 
 
 # Ф-ция Заново
-def reset_file(filename_: str = "config_logs.json"):
-    os.remove(os.path.join(filename_))
-    stat_logos(clear=True)
+def reset_file():
+    data = read_json_file(filename_="config.json")
+    write_json_file(data=data)
+    clear_logs(clear=True)
 
 
 ###################################################################################################################
 # Алгоритм NEXT
 def next_rate(filename_: str):
-    dict_data = read_json_file(filename_)
+    data = read_json_file(filename_)
+    rate_, money_usd_, money_uah_ = data_read(data)
     delta_range = random.triangular(-0.5, 0.5, 0.01)
-    rate_ = dict_data["rate"]
     new_rate = round((rate_ + delta_range), 2)
     delta_ = round(delta_range, 2)
-    money_usd = dict_data["amount on account USD"]
-    money_uah = dict_data["amount on account UAH"]
-    dict_data["rate"] = new_rate
-    dict_data["delta"] = delta_
-    write_json_file(dict_data)
-    stat_logos(rate_=new_rate, delta_=delta_, money_usd_=money_usd, money_uah_=money_uah)
+    data = data_write(data_=data, rate_=new_rate, money_usd_=money_usd_, money_uah_=money_uah_, delta=delta_)
+    write_json_file(data=data)
+    stat_logos(rate_=new_rate, delta_=delta_, money_usd_=money_usd_, money_uah_=money_uah_)
 
 
 ###################################################################################################################
@@ -195,11 +197,8 @@ commands = {'NEXT': next_rate,
 #####################################################################################################################
 # Запуск сценариев
 if keys in ("NEXT", "RATE", "AVAILABLE"):
-    filename = "config_logs.json"
-    if not os.path.isfile(os.path.join(filename)):
-        filename = "config.json"
     play_sc = commands[keys]
-    play_sc(filename_=filename)
+    play_sc(filename_="config_logs.json")
 elif keys in ("BUY ALL", "SELL ALL", "RESTART"):
     play_sc = commands[keys]
     play_sc()
